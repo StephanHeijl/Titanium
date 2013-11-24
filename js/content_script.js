a1a578c53c48d663f9dea3d823732ab6857211cb
StephanHeijl <me@stephanheijl.com>
2013-11-24 03:38:30 +0100

    Added succesful highlighter
    Hover, click interaction
    image verifcation for IMG tags, anchor lookups and background images.

--- js/content_script.js
+++ js/content_script.js
@@ -3,38 +3,134 @@ $(function () {
 		function (request, sender, sendResponse) {
 		console.log(request);
 		if (request.highlight == 1) {
+			var highlighter = $("<div>");
+			var highlighterData = $("<span>");
+
+			highlighter.addClass("titanium-highlight");
+			highlighterData.addClass("titanium-highlight-data");
+
+			highlighter.append(highlighterData);
+
+			$("body").append(highlighter);
+
 			console.log("Highlighting element");
-			
-			document.body.addEventListener("mouseover", t, false);
-			
-			
-			$("body *").click(function (event) {
+			$("body").on("mouseover", null, function (event) {
+				var t = $(event.target);
+				var offset = t.offset();
+				highlighter.css({
+					"width" : t.outerWidth(),
+					"height" : t.outerHeight(),
+					"left" : offset.left,
+					"top" : offset.top
+				});
+
+				var tagName = t.prop("tagName");
+				if (tagName == "A" || tagName == "IMG") {
+					highlighter.css({
+						"border-color" : "#2E7EFF"
+					});
+					highlighterData.css({
+						"background-color" : "#2E7EFF"
+					});
+				} else {
+					highlighter.css({
+						"border-color" : "#333"
+					});
+					highlighterData.css({
+						"background-color" : "#333"
+					});
+				}
+
+				highlighterData.html("<b>" + tagName + "</b> ")
+				if (typeof t.attr("src") !== 'undefined' && t.attr("src") !== false) {
+					highlighterData.append("src: " + t.attr("src") + " ")
+				}
+
+				if (typeof t.attr("href") !== 'undefined' && t.attr("href") !== false) {
+					highlighterData.append("href: " + t.attr("href") + " ")
+				}
+
+			});
+
+			$("body").on("click", null, function (event) {
 				event.preventDefault();
-				$(".titanium-highlight").removeClass("titanium-highlight");
-				console.log($(this));
-				$("body").off("mouseleave", "*");
-				$("body").off("mouseover", null);
+
+				var t = $(event.target);
+				var imgUrl = "";
+
+				if (t.prop("tagName") == "IMG") {
+					// We'll look for a parent anchor on top of the img,
+					// as these will often be links to larger versions of the image
+
+					pu = t.parentsUntil("a");
+					anchor = pu.eq(pu.length - 1).parent();
+					console.log(pu);
+					console.log(anchor);
+
+					if (anchor.prop("tagName") != "A") {
+
+						// We don't want to save the link if it's not an image, so we check whether it contains
+						// one of these common image extensions
+						var href = anchor.attr("href");
+						var extensions = [".jpg", ".png", ".gif", ".jpeg", ".tif", ".bmp", ".raw"];
+						var foundExt = false;
+
+						for (e in extensions) {
+							ext = extensions[e];
+							if (href.indexOf(ext) > 0) {
+								imgUrl = href;
+								foundExt = true;
+								break;
+							}
+						}
+						if (!foundExt) {
+							imgUrl = t.attr("src");
+						}
+					} else {
+						imgUrl = t.attr("src");
+					}
+				} else if (t.prop("tagName") == "A") {
+					// Same logic as before, except we'll stop if we can't find anything we can use.
+					var href = t.attr("href");
+					var extensions = [".jpg", ".png", ".gif", ".jpeg", ".tif", ".bmp", ".raw"];
+					var foundExt = false;
+
+					for (e in extensions) {
+						ext = extensions[e];
+						if (href.indexOf(ext) > 0) {
+							imgUrl = href;
+							foundExt = true;
+							break;
+						}
+					}
+					if (!foundExt) {
+						// We can get the element background, if there is one...
+						var bgImg = t.css('background-image');
+						console.log(bgImg);
+						bgImg = bgImg.replace('url(', '').replace(')', '');
+						imgUrl = bgImg;
+					}
+				} else {
+					// We can get the element background, if there is one...
+					var bgImg = t.css('background-image');
+					console.log(bgImg);
+					bgImg = bgImg.replace('url(', '').replace(')', '');
+					
+					imgUrl = bgImg;
+				}
+
+				console.log(imgUrl)
+				if (imgUrl.length > 0 && imgUrl != "none") {
+					sendResponse({
+						"imgUrl" : imgUrl
+					});
+				} else {
+					alert("Could not retreive an image from this element.");
+				}
+
+				//$(".titanium-highlight").remove()
+				$("body").off("mouseover click", null);
 			});
 		}
 	});
-	
-	var e;
-	
-	function t(t) {
-		if (t.target === document.body || e && e === t.target) {
-			return
-		}
-		if (e) {
-			//$(e).removeClass("titanium-highlight");
-			//$(".titanium-highlight").remove()
-			e.style.border = ""
-			e = undefined
-		}
-		if (t.target) {
-			e = t.target;
-			$(e).append("<div class='titanium-highlight'></div>");
-			console.log($(e));
-			e.style.border = "1px solid blue"
-		}
-	}
 });
